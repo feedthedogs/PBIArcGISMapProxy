4b8a926b21bdd77aef6777dd9efbe6afe1074ce6
feedthedogs <feedthedogs@hotmail.com>
2018-08-19 22:57:45 +1000

    initial commit

---
+++ PBIProxy/PBIStaticFiles/PrepareFilesAndCertificate.ps1
@@ -0,0 +1,41 @@
+﻿###### Create a new Certificate to impersonate ArcGIS' domains
+
+# This is for testing purposes, you should use a CA to sign a certificate and keep it in the Certificate Manager
+# Create a selfsigned certficiate with the list of domains
+$cert = New-SelfSignedCertificate -FriendlyName "PBIProxy" -KeyLength 2048 -certstorelocation Cert:\LocalMachine\My -DnsName "lacdn.arcgis.com", "www.arcgis.com","basemaps.arcgis.com","utility.arcgis.com","static.arcgis.com"
+# Move it to be trusted as a Root CA
+Move-Item (Join-Path Cert:\LocalMachine\My $cert.Thumbprint) -Destination Cert:\LocalMachine\Root
+
+# Export the certificate so that the webserver can use it
+$mypwd = ConvertTo-SecureString -String "Password01$" -Force -AsPlainText
+Get-ChildItem -Path (Join-Path Cert:\LocalMachine\Root $cert.Thumbprint) | Export-PfxCertificate -FilePath ..\PBICert.pfx -Password $mypwd
+
+
+###### Download Static PowerBI ArcGIS files
+
+# Have the requests look similar to PowerBI
+$userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.122 Safari/537.36'
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add('Accept','*/*')
+$headers.Add('Referer','https://app.powerbi.com/')
+$headers.Add('Origin','ms-pbi://pbi.microsoft.com')
+
+# Get the list of Static files that are not provided by ArcGis offline
+$root = (Get-Item -Path ".\").FullName
+$urls = Get-Content "$root\Urls.txt"
+foreach($url in $urls) {
+    # Build the directories to match the paths that PowerBI requests
+    $path = $url.Split('/')
+    $buildpath = '\'
+    for ($i = 3; $i -lt $path.Count - 1; $i++) {
+        # Write-Output "$root$($buildpath)$($path[$i])"
+        md -Path "$root$($buildpath)$($path[$i])" -ErrorAction SilentlyContinue
+        $buildpath += "$($path[$i])\"
+    }
+    # Querystrings are appended to the end of the filename with an underscore
+    $filename = $path[$path.Count - 1].Replace('?','_')
+    
+    # Get the file and write it to a folder to serve from
+    Write-Output "Downloading: $root$($buildpath)$filename"
+    Invoke-WebRequest $url -OutFile "$root$($buildpath)$filename" -UserAgent $userAgent -Headers $headers
+}
---
+++ PBIProxy/Startup.cs
@@ -0,0 +1,129 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+
+namespace PBIProxy
+{
+    public class Startup
+    {
+        static HttpClient _httpClient { get; set; }
+        static string _geoCodeServerURL { get; set; }
+        static string _tileServerURL { get; set; }
+        static string _pbiTileServerURL { get; set; }
+
+        public Startup(IConfiguration configuration)
+        {
+            _tileServerURL = configuration["TileServerURL"];
+            _pbiTileServerURL = configuration["PBITileServerURL"];
+            _geoCodeServerURL = configuration["GeoCodeServerURL"];
+
+            var proxy = new WebProxy()
+            {
+                Address = new Uri("http://192.168.2.7:8888")
+            };
+            var httpClientHandler = new HttpClientHandler()
+            {
+                Proxy = proxy,
+            };
+            _httpClient = new HttpClient(httpClientHandler, false);
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+        
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.Map("/usrsvcs", HandleGeoCodeServer);
+            app.Map("/arcgis", HandleTileMapServer);
+
+            app.Use(async (context, next) =>
+            {
+                // Query strings have been converted to static files trailing with an underscore and the filename
+                if (context.Request.QueryString.Value != "")
+                {
+                    context.Request.Path = string.Format("{0}_{1}", context.Request.Path.Value, context.Request.QueryString.Value.Substring(1));
+                }
+                await next();
+            });
+
+            // for each of the external folders make them accessible in the root folder
+            var externalFolder = Path.Combine(Directory.GetCurrentDirectory(), "PBIStaticFiles");
+            foreach (var folder in Directory.EnumerateDirectories(externalFolder))
+            {
+                app.UseStaticFiles(new StaticFileOptions
+                {
+                    OnPrepareResponse = ctx => {
+                        ctx.Context.Response.Headers.Append("Access-Control-Allow-Origin", "ms-pbi://pbi.microsoft.com");
+                    },
+                    FileProvider = new PhysicalFileProvider(folder),
+                    RequestPath = string.Format("/{0}", new DirectoryInfo(folder).Name),
+                    ServeUnknownFileTypes = true,
+                    DefaultContentType = "application/json"
+                });
+            }
+        }
+
+        private static void HandleGeoCodeServer(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                // Translate from the Request.From to a KeyValuePair collection
+                var formData = new List<KeyValuePair<string, string>>();
+                foreach (var k in context.Request.Form.Keys)
+                {
+                    formData.Add(new KeyValuePair<string, string>(k, context.Request.Form[k]));
+                }
+
+                // fetch the response from the configured source
+                var postContent = new FormUrlEncodedContent(formData);
+                var response = await _httpClient.PostAsync(_geoCodeServerURL, postContent);
+
+                string content = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    context.Response.Headers.Add("Access-Control-Allow-Origin", "ms-pbi://pbi.microsoft.com");
+                    context.Response.ContentType = response.Content.Headers.ContentType.ToString();
+                    await context.Response.WriteAsync(content);
+                }
+                else
+                {
+                    await context.Response.WriteAsync("An error occurred: " + content);
+                }
+            });
+        }
+
+        private static void HandleTileMapServer(IApplicationBuilder app)
+        {
+            app.Run(async context =>
+            {
+                // fetch the response from the configured source
+                var ExtraURL = context.Request.Path.Value.Replace(_pbiTileServerURL, "");
+                var requestURL = $"{_tileServerURL}{ExtraURL}{context.Request.QueryString.Value}";
+                var response = await _httpClient.GetAsync(requestURL);
+
+                string content = await response.Content.ReadAsStringAsync();
+                if (response.IsSuccessStatusCode)
+                {
+                    context.Response.Headers.Add("Access-Control-Allow-Origin", "ms-pbi://pbi.microsoft.com");
+                    context.Response.ContentType = response.Content.Headers.ContentType.ToString();
+                    context.Response.Headers.Add("Cache-Control", response.Headers.CacheControl.ToString());
+                    context.Response.Headers.Add("Last-Modified", response.Content.Headers.LastModified.ToString());
+                    await context.Response.WriteAsync(content);
+                }
+                else
+                {
+                    await context.Response.WriteAsync("An error occurred: " + content);
+                }
+            });
+        }
+    }
+}
